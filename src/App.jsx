fc4884cd761d6313dff4fd3a4b6b184fe8588ed6
Adam Skoczeń <adamsk91@wp.pl>
2024-10-02 19:00:11 +0200

    12

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        setData(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Ładowanie...</p>;
+  if (error) return <p>Błąd: {error.message}</p>;
+
+  return (
+    <ul>
+      {data.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default App;
